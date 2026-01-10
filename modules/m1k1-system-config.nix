64dec1ef7e8cb39e1b59a16c2882c8de0f31afe7
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-10 17:19:07 +0000

    switched to vim-full for clipboard integration

--- modules/m1k1-system-config.nix
+++ modules/m1k1-system-config.nix
@@ -76,7 +76,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim-full # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
   ];
