18b15410b86e4b8a8c7a702ddab6421fa6893587
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-01-10 22:56:45 +0000

    cleanup

--- flake.nix
+++ flake.nix
@@ -6,33 +6,35 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-		
-		mikoshi-vim = {
-			#url = "github:szewczyk-bartosz/mikoshi-neovim";
-			url = "path:/home/cheryllamb/mikoshi-neovim";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+
+    mikoshi-vim = {
+      url = "github:szewczyk-bartosz/mikoshi-neovim";
+      #url = "path:/home/cheryllamb/mikoshi-neovim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, mikoshi-vim }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          modules/m1k1-system-config.nix
-					mikoshi-vim.nixosModules.default
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    mikoshi-vim,
+  }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        modules/m1k1-system-config.nix
+        mikoshi-vim.nixosModules.default
 
-          home-manager.nixosModules.home-manager 
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cheryllamb = import ./modules/home.nix;
-          }
-        ];
-      };
-
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.cheryllamb = import ./modules/home.nix;
+        }
+      ];
     };
+  };
 }
