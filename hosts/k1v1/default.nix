92bfef416277525b55eda4ed8c298ad442dde72b
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-10 22:18:34 +0000

    updated home-manager settings

--- hosts/k1v1/default.nix
+++ hosts/k1v1/default.nix
@@ -5,8 +5,12 @@
     ../../modules/shared-system.nix
   ];
 
-  home-manager.users.cheryllamb = {
-    home.stateVersion = "26.05";
-    imports = [../../modules/shared-home.nix];
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.cheryllamb = {
+      home.stateVersion = "26.05";
+      imports = [../../modules/shared-home.nix];
+    };
   };
 }
--- hosts/m1k1/default.nix
+++ hosts/m1k1/default.nix
@@ -1,12 +1,16 @@
-{ ... }: {
+{...}: {
   imports = [
     ./hardware.nix
     ./system.nix
     ../../modules/shared-system.nix
   ];
 
-  home-manager.users.cheryllamb = {
-    home.stateVersion = "26.05";
-    imports = [ ../../modules/shared-home.nix ];
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.cheryllamb = {
+      home.stateVersion = "26.05";
+      imports = [../../modules/shared-home.nix];
+    };
   };
 }
