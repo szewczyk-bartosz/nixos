f325ea0b642fe282376af3f108c7cec867662d0b
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-10 22:15:17 +0000

    hostname and formatting

--- hosts/t3kl4/system.nix
+++ hosts/t3kl4/system.nix
@@ -1,13 +1,18 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
+  networking.hostName = "t3kl4";
 
   users.users.cheryllamb = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ ];
+    extraGroups = ["wheel"];
+    packages = with pkgs; [];
   };
 
   system.stateVersion = "25.11";
