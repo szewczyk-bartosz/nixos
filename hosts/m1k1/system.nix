8a7add47493bcaf5d9fb06af78753612f9d9259f
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-10 22:15:11 +0000

    gamemode and correct hostname

--- hosts/m1k1/system.nix
+++ hosts/m1k1/system.nix
@@ -1,13 +1,18 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
+  networking.hostName = "m1k1";
 
   users.users.cheryllamb = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ ];
+    extraGroups = ["wheel" "gamemode"];
+    packages = with pkgs; [];
   };
 
   system.stateVersion = "25.11";
