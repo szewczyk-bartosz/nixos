e712b550ecc6089d43b18ddddde447767d07c8ca
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-10 22:21:45 +0000

    updated the flake structure to match the new design

--- flake.nix
+++ flake.nix
@@ -19,55 +19,22 @@
     nixpkgs,
     home-manager,
     mikoshi,
-    mikoshi-vim,
-  }: let
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      inherit system;
+  }: {
+    nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
-        modules/nixos-system-config.nix
-        mikoshi.modules.nixos.gnomoshi
-        mikoshi-vim.nixosModules.default
         home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.cheryllamb = {
-            home.stateVersion = "26.05";
-            imports = [./modules/home.nix];
-          };
-        }
+        mikoshi.modules.nixos.gnomoshi
+        ./hosts/m1k1
       ];
     };
-    nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        modules/m1k1-hardware.nix
-        mikoshi.modules.nixos.gnomoshi
-        mikoshi-vim.nixosModules.default
-        home-manager.nixosModules.home-manager
-        {
-          # Config file flattened
-          boot.loader.systemd-boot.enable = true;
-          boot.loader.efi.canTouchEfiVariables = true;
-          networking.hostName = "m1k1";
-          users.users.cheryllamb = {
-            isNormalUser = true;
-            extraGroups = ["wheel" "gamemode"];
-          };
-          system.stateVersion = "25.11";
-          nix.settings.experimental-features = ["nix-command" "flakes"];
 
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.cheryllamb = {
-            home.stateVersion = "26.05";
-            imports = [
-              ./modules/home.nix
-            ];
-          };
-        }
+    nixosConfigurations.k1v1 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        home-manager.nixosModules.home-manager
+        mikoshi.modules.nixos.gnomoshi
+        ./hosts/k1v1
       ];
     };
   };
