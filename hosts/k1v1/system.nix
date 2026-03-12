f36bb7f252e0a161c8e0e24d8262532d9ef61666
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-03-12 20:08:49 +0000

    gaming optimisations for k1v1

--- hosts/k1v1/system.nix
+++ hosts/k1v1/system.nix
@@ -11,8 +11,22 @@
 
   users.users.cheryllamb = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel" "gamemode"];
     packages = with pkgs; [];
+  };
+
+  # Optimisations specific to my laptop
+  hardware.graphics.enable = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver
+    intel-vaapi-driver
+    intel-compute-runtime
+  ];
+  programs.gamemode.enable = true;
+  boot.kernel.sysctl."kernel.sched_itmt_enabled" = 1;
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+    DXVK_ASYNC = "1";
   };
 
   system.stateVersion = "25.11";
