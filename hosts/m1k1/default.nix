3bc08af0a1de2fb9ed07ad83ac46f5eac2b90260
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-06-29 01:20:35 +0100

    workspace 3 is a gaming workspace nowww

--- hosts/m1k1/default.nix
+++ hosts/m1k1/default.nix
@@ -66,7 +66,12 @@ in {
           "match:class ^([Ss]potify)$, workspace special:music, float on, size 2200  1100, center on"
           "match:class ^([Ss]team)$, workspace special:steam, float on, size 2200 1100, center on"
           "no_blur on, match:class ^([Aa]wakened-poe-trade)$"
-          "match:class steam_app_.*, float on"
+          "match:class steam_app_.*, workspace 3, float off, fullscreen on"
+        ];
+
+        workspace = [
+          "3, gapsin:0, gapsout:0, bordersize:0, rounding:0, decorate:false"
+          # "3, defaultName:gaming, gapsin:0, gapsout:0, border:false, rounding:false, decorate:false"
         ];
 
         bind = [
