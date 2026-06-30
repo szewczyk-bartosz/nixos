824090af4cdbb5c0f4e60519f6abb4c4ffe84ca9
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-06-30 23:47:52 +0100

    refactor

--- hosts/m1k1/default.nix
+++ hosts/m1k1/default.nix
@@ -76,32 +76,7 @@ in {
     users.cheryllamb = {
       home.stateVersion = "26.05";
       programs.mangohud.enable = true;
-      wayland.windowManager.hyprland.settings = {
-        "exec-once" = [
-          "spotify"
-          "steam"
-        ];
-
-        # render.direct_scanout = lib.mkForce 1;
-
-        windowrule = [
-          "match:class ^([Ss]potify)$, workspace special:music, float on, size 2200  1100, center on"
-          "match:class ^([Ss]team)$, workspace special:steam, float on, size 2200 1100, center on"
-          "no_blur on, match:class ^([Aa]wakened-poe-trade)$"
-          "match:class steam_app_.*, workspace 3, float off, fullscreen on"
-        ];
-
-        workspace = [
-          "3, gapsin:0, gapsout:0, bordersize:0, rounding:0, decorate:false"
-          # "3, defaultName:gaming, gapsin:0, gapsout:0, border:false, rounding:false, decorate:false"
-        ];
-
-        bind = [
-          "$mainMod, S, togglespecialworkspace, music"
-          "$mainMod, W, togglespecialworkspace, steam"
-        ];
-      };
-      imports = [../../modules/shared-home.nix];
+      imports = [../../modules/shared-home.nix ./hyprland.nix];
     };
   };
 
