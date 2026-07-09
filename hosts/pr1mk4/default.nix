d5147b7bc662084a963915c590f5c3a08f173287
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 16:08:18 +0100

    pr1mk4 refactored

--- hosts/pr1mk4/default.nix
+++ hosts/pr1mk4/default.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  system.stateVersion = "25.11";
   imports = [
     ./hardware.nix
     ../../modules/shared-system.nix
@@ -6,30 +7,21 @@
     ../../modules/devTools.nix
   ];
 
+  # BOOT
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "pr1mk4";
+  # SYSTEM PACKAGES
+  environment.systemPackages = with pkgs; [];
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "Europe/London";
-
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWIO068BP8YipXaSHkjJL/xzyv2PBfveoXt5Z9GsSKM cheryllamb@m1k1"
-  ];
-
+  # USERS
   users.users.cheryllamb = {
     isNormalUser = true;
     extraGroups = ["wheel" "gamemode"];
     packages = with pkgs; [];
   };
 
-  hardware.graphics.enable = true;
-  programs.gamemode.enable = true;
-
-  nix.settings.trusted-users = ["root" "cheryllamb"];
-
+  # HOME MANAGER
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
@@ -39,5 +31,15 @@
     };
   };
 
-  system.stateVersion = "25.11";
+  # NETWORKING
+  networking.hostName = "pr1mk4";
+  services.openssh.enable = true;
+
+  # OTHER
+  # users.users.root.openssh.authorizedKeys.keys = [
+  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWIO068BP8YipXaSHkjJL/xzyv2PBfveoXt5Z9GsSKM cheryllamb@m1k1"
+  # ];
+  # nix.settings.trusted-users = ["root" "cheryllamb"];
+
+  # OVERLAYS
 }
