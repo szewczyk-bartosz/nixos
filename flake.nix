4faae1e94c573a17aa7b81e9029264b9f44baac2
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-07-09 16:40:44 +0100

    done!

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
 {
   inputs = {
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,42 +10,52 @@
       # url = "path:/home/cheryllamb/mikoshi";
     };
 
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.follows = "mikoshi/nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     mikoshi,
     disko,
   }: {
     nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        mikoshi.nixosModules.mikoshi-hyprland
-        mikoshi.nixosModules.bmd
-        disko.nixosModules.disko
-        mikoshi.nixosModules.steam
-        home-manager.nixosModules.home-manager
-        {
-          mikoshi.stylix.base16Scheme = "catppuccin-mocha";
-          mikoshi.hyprland.monitors = ["DP-2,2560x1440@240,0x0,1"];
-          mikoshi.hyprland.kb = "gb,ua,pl";
-          mikoshi.hyprland.wallpaper = ./wallpapers/nixos.png;
-        }
-        ./hosts/m1k1
-      ];
+      specialArgs = {inherit nixpkgs-unstable;};
+      modules =
+        [
+          disko.nixosModules.disko
+          ./hosts/m1k1
+        ]
+        ++ (with mikoshi.modules.nixos; [
+          gnome
+          gaming
+          bmd
+          nvim
+        ]);
+    };
+
+    nixosConfigurations.t3kl4 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          disko.nixosModules.disko
+          ./hosts/t3kl4
+        ]
+        ++ (with mikoshi.modules.nixos; [
+          base
+          nvim
+          bmd
+        ]);
     };
 
     nixosConfigurations.pr1mk4 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        mikoshi.nixosModules.mikoshi-gnome
         home-manager.nixosModules.home-manager
-        {
-          mikoshi.stylix.base16Scheme = "catppuccin-mocha";
-        }
         ./hosts/pr1mk4
       ];
     };
@@ -58,54 +63,17 @@
     nixosConfigurations.k1v1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        mikoshi.nixosModules.mikoshi-hyprland
-        mikoshi.nixosModules.bmd
         disko.nixosModules.disko
-        mikoshi.nixosModules.steam
-        home-manager.nixosModules.home-manager
-        {
-          mikoshi.stylix.base16Scheme = "catppuccin-mocha";
-          mikoshi.hyprland.kb = "gb,ua,pl";
-          mikoshi.hyprland.wallpaper = ./wallpapers/nixos.png;
-        }
         ./hosts/k1v1
-      ];
-    };
-
-    nixosConfigurations.t3kl4 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        home-manager.nixosModules.home-manager
-        disko.nixosModules.disko
-        mikoshi.nixosModules.neovim
-        mikoshi.nixosModules.bmd
-        mikoshi.nixosModules.tmux
-        ./hosts/t3kl4
       ];
     };
 
     nixosConfigurations.virt = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        home-manager.nixosModules.home-manager
         disko.nixosModules.disko
         ./hosts/virt
-
-        mikoshi.nixosModules.ghostty
-        mikoshi.nixosModules.audio
-        mikoshi.nixosModules.fonts
-        mikoshi.nixosModules.icons
-        mikoshi.nixosModules.neovim
-        mikoshi.nixosModules.network
-        mikoshi.nixosModules.tmux
       ];
-    };
-
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      shellHook = ''
-        [ -f .env ] && source .env
-        alias deploy-t3kl4="nixos-rebuild switch --flake .#t3kl4 --target-host cheryllamb@$T3KL4_IP --sudo --ask-sudo-password"
-      '';
     };
   };
 }
