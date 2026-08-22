247643f48d6a006f9449c3170fc2d70a71c28d50
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-08-22 17:40:41 +0100

    pr1mk4 done, I need to review the list of hosts again as its gettting a bit messy

--- hosts/pr1mk4/default.nix
+++ hosts/pr1mk4/default.nix
@@ -1,4 +1,10 @@
-{pkgs, lib, nixpkgs-unstable, ...}: {
+{
+  pkgs,
+  lib,
+  nixpkgs-unstable,
+  ...
+}:
+{
   system.stateVersion = "25.11";
   imports = [
     ./hardware.nix
@@ -12,25 +18,37 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # MIKOSHI OPTIONS
-  mikoshi.meta = {
-    keyboardLayouts = ["gb" "pl" "ua"];
-    users = ["cheryllamb"];
+  mikoshi = {
+    meta = {
+      keyboardLayouts = [
+        "gb"
+        "pl"
+        "ua"
+      ];
+      users = [ "cheryllamb" ];
+    };
+
+    stylix = {
+      enable = true;
+      base16Scheme = "catppuccin-mocha";
+    };
+    
+    hyprland.enable = true;
+    # waybar.battery.enable = true;
+    hyprland.wallpaper = ../../wallpapers/nixos.png;
   };
-  mikoshi.waybar.battery.enable = true;
-  mikoshi.stylix = {
-    enable = true;
-    base16Scheme = "catppuccin-mocha";
-  };
-  mikoshi.hyprland.wallpaper = ../../wallpapers/nixos.png;
 
   # SYSTEM PACKAGES
-  environment.systemPackages = with pkgs; [];
+  environment.systemPackages = with pkgs; [ ];
 
   # USERS
   users.users.cheryllamb = {
     isNormalUser = true;
-    extraGroups = ["wheel" "gamemode"];
-    packages = with pkgs; [];
+    extraGroups = [
+      "wheel"
+      "gamemode"
+    ];
+    packages = with pkgs; [ ];
   };
 
   # HOME MANAGER
@@ -39,7 +57,7 @@
     useUserPackages = true;
     users.cheryllamb = {
       home.stateVersion = "26.05";
-      imports = [../../modules/shared-home.nix];
+      imports = [ ../../modules/shared-home.nix ];
     };
   };
 
