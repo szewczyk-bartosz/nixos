7d15e82d2043b9da55badf72e6b481647540965a
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-08-26 18:02:06 +0100

    formatting

--- flake.nix
+++ flake.nix
@@ -11,50 +11,52 @@
     };
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "mikoshi/nixpkgs"; }; outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    mikoshi,
-    disko,
-  }: {
-    nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit nixpkgs-unstable;};
-      modules =
-        [
+    nixpkgs.follows = "mikoshi/nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      mikoshi,
+      disko,
+    }:
+    {
+      nixosConfigurations.m1k1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit nixpkgs-unstable; };
+        modules = [
           disko.nixosModules.disko
           mikoshi.modules.nixos.default
           ./hosts/m1k1
         ];
-    };
+      };
 
-    nixosConfigurations.t3kl4 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit nixpkgs-unstable;};
-      modules =
-        [
+      nixosConfigurations.t3kl4 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit nixpkgs-unstable; };
+        modules = [
           disko.nixosModules.disko
           mikoshi.modules.nixos.default
           ./hosts/t3kl4
         ];
-    };
+      };
 
-    nixosConfigurations.k1v1 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit nixpkgs-unstable;};
-      modules = [
-        mikoshi.modules.nixos.default
-        ./hosts/k1v1
-      ];
-    };
+      nixosConfigurations.k1v1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit nixpkgs-unstable; };
+        modules = [
+          mikoshi.modules.nixos.default
+          ./hosts/k1v1
+        ];
+      };
 
-    nixosConfigurations.virt = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        ./hosts/virt
-      ];
+      nixosConfigurations.virt = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./hosts/virt
+        ];
+      };
     };
-  };
 }
