9b92e438b3105e970e047abc82b22206d31dc203
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-08-27 15:05:25 +0100

    temp disk config, to be swapped once I know the specific disk layout for new laptop

---
+++ hosts/k1v1/disk.nix
@@ -0,0 +1,55 @@
+{
+  disko.devices = {
+    main = {
+      type = "disk";
+      device = "/dev/nvme1n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            priority = 1;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "fmask=0077"
+                "dmask=0077"
+              ];
+              extraArgs = [
+                "-n"
+                "NIXBOOT"
+              ];
+            };
+          };
+          swap = {
+            size = "40G";
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+              resumeDevice = true;
+              extraArgs = [
+                "-L"
+                "NIXSWAP"
+              ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              extraArgs = [
+                "-L"
+                "NIXROOT"
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+}
