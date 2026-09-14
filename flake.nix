4f84555f7b48aa4169bd843c9bfbc4b1b01be45f
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-09-14 17:54:32 +0100

    formatter fixed!

--- flake.nix
+++ flake.nix
@@ -33,13 +33,21 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
+      shellHook = ''
+        echo "Dots Dev Shell Loaded"
+        echo "run with: deploy-engram"
+      '';
       packages = [
         (pkgs.writeShellScriptBin "deploy-engram" ''
           set -e
           nix flake update engram
-          git add flake.lock
-          git commit -m "bumped engram"
-          nixos-rebuild switch --flake .#t3kl4 --target-host cheryllamb@t3kl4 --build-host localhost --use-remote-sudo
+          if git diff --quiet flake.lock; then
+            echo "engram already up to date, skipping commit"
+          else
+            git add flake.lock
+            git commit -m "bumped engram"
+          fi
+          nixos-rebuild switch --flake .#t3kl4 --target-host cheryllamb@t3kl4 --ask-sudo-password --sudo
         '')
       ];
     };
